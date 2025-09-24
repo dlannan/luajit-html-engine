e02ffdd432a93e8b401acd5c3bdfff78971684b4
dlannan <dlannan_temp@gagagames.com>
2025-09-25 01:14:32 +0930

    Input and test 10 to finish

--- temp_dom.lua
+++ temp_dom.lua
@@ -2,446 +2,86 @@
   [1] =  {
     [1] =  {
       [1] =  {
-        [1] = Page Title
-        ["dirty"] = true
+        ["label"] = title
         ["xarg"] =  {
         }
-        ["label"] = title
         ["id"] = 4
       }
-      [2] =  {
-        [1] = body { background-color: orange;}
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = style
-        ["id"] = 7
-      }
+      ["label"] = head
       ["dirty"] = true
       ["xarg"] =  {
       }
-      ["label"] = head
       ["id"] = 3
     }
     [2] =  {
       [1] =  {
-        [1] =  {
-          [1] = Heading 1
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = b
-          ["id"] = 10
-        }
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = h1
-        ["id"] = 9
-      }
-      [2] =  {
-        [1] = Heading 1
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = h1
-        ["id"] = 12
-      }
-      [3] =  {
-        [1] = For 50 years, WWF has been protecting the future of nature. The world's leading conservation organization, WWF works in 100 countries and is supported by 1.2 million members in the United States and close to 5 million globally.
-        ["id"] = 14
-        ["dirty"] = true
-        ["label"] = blockquote
-        ["xarg"] =  {
-          ["cite"] = http://www.worldwildlife.org/who/index.html
-        }
-      }
-      [4] =  {
-        [1] = Heading 2
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = h2
-        ["id"] = 16
-      }
-      [5] =  {
-        [1] = He
+        [1] = This is 
         [2] =  {
-          [1] =  {
-            [1] = ad
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = i
-            ["id"] = 21
-          }
+          [1] = bold
+          ["label"] = b
           ["dirty"] = true
           ["xarg"] =  {
           }
-          ["label"] = b
+          ["id"] = 8
+        }
+        [3] = , 
+        [4] =  {
+          [1] = strong
+          ["label"] = strong
+          ["dirty"] = true
+          ["xarg"] =  {
+          }
+          ["id"] = 11
+        }
+        [5] = , 
+        [6] =  {
+          [1] = italic
+          ["label"] = i
+          ["dirty"] = true
+          ["xarg"] =  {
+          }
+          ["id"] = 14
+        }
+        [7] = , 
+        [8] =  {
+          [1] = emphasis
+          ["label"] = em
+          ["dirty"] = true
+          ["xarg"] =  {
+          }
+          ["id"] = 17
+        }
+        [9] = , 
+        [10] =  {
+          [1] = underline
+          ["label"] = u
+          ["dirty"] = true
+          ["xarg"] =  {
+          }
           ["id"] = 20
         }
-        [3] = ing 3
-        ["id"] = 18
-        ["dirty"] = true
-        ["label"] = h3
-        ["xarg"] =  {
-        }
-      }
-      [6] =  {
-        [1] = H
-        [2] =  {
-          [1] = ead
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = b
-          ["id"] = 26
-        }
-        [3] = ing 4
-        ["id"] = 24
-        ["dirty"] = true
-        ["label"] = h4
-        ["xarg"] =  {
-        }
-      }
-      [7] =  {
-        [1] = Heading 5
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = h5
-        ["id"] = 29
-      }
-      [8] =  {
-        [1] =  {
-          [1] = Heading
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = i
-          ["id"] = 32
-        }
-        [2] =  6
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = h6
-        ["id"] = 31
-      }
-      [9] =  {
-        [1] = My first paragraph.
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
+        [11] = .
         ["label"] = p
-        ["id"] = 35
-      }
-      [10] =  {
-        [1] = My first paragraph
-        [2] =  {
-          ["xarg"] =  {
-          }
-          ["label"] = br
-          ["id"] = 39
-        }
-        [3] = with a break in it.
-        ["id"] = 37
-        ["dirty"] = true
-        ["label"] = p
+        ["id"] = 6
         ["xarg"] =  {
-        }
-      }
-      [11] =  {
-        [1] =  {
-          [1] =  {
-            [1] = Company
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = th
-            ["id"] = 43
-          }
-          [2] =  {
-            [1] = Contact
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = th
-            ["id"] = 45
-          }
-          [3] =  {
-            [1] = Country
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = th
-            ["id"] = 47
-          }
-          [4] =  {
-            [1] = Number
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = th
-            ["id"] = 49
-          }
-          ["id"] = 42
-          ["dirty"] = true
-          ["label"] = tr
-          ["xarg"] =  {
-          }
-        }
-        [2] =  {
-          [1] =  {
-            [1] = Alfreds Futterkiste
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 52
-          }
-          [2] =  {
-            [1] = Maria Anders
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 54
-          }
-          [3] =  {
-            [1] = Germany
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 56
-          }
-          [4] =  {
-            [1] = 219784
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 58
-          }
-          ["id"] = 51
-          ["dirty"] = true
-          ["label"] = tr
-          ["xarg"] =  {
-          }
-        }
-        [3] =  {
-          [1] =  {
-            [1] = Centro comercial Moctezuma
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 61
-          }
-          [2] =  {
-            [1] = Francisco Chang
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 63
-          }
-          [3] =  {
-            [1] = Mexico
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 65
-          }
-          [4] =  {
-            [1] = 2
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 67
-          }
-          ["id"] = 60
-          ["dirty"] = true
-          ["label"] = tr
-          ["xarg"] =  {
-          }
-        }
-        [4] =  {
-          [1] =  {
-            [1] = Capital Aspico
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 70
-          }
-          [2] =  {
-            [1] = Jimme Phu
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 72
-          }
-          [3] =  {
-            [1] = United States of America
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 74
-          }
-          [4] =  {
-            [1] = 170
-            ["dirty"] = true
-            ["xarg"] =  {
-            }
-            ["label"] = td
-            ["id"] = 76
-          }
-          ["id"] = 69
-          ["dirty"] = true
-          ["label"] = tr
-          ["xarg"] =  {
-          }
-        }
-        ["id"] = 41
-        ["dirty"] = true
-        ["label"] = table
-        ["xarg"] =  {
-        }
-      }
-      [12] =  {
-        [1] =  A button 
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = button
-        ["id"] = 78
-      }
-      [13] = Test
-      [14] =  {
-        [1] =  {
-          [1] =  B button 
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = i
-          ["id"] = 82
         }
         ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = button
-        ["id"] = 81
       }
-      [15] = Test
-      [16] =  {
-        [1] =  {
-          [1] =  C button 
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = i
-          ["id"] = 86
-        }
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = button
-        ["id"] = 85
-      }
-      [17] = Test
-      [18] =  {
-        [1] =  {
-          [1] =  D button 
-          ["dirty"] = true
-          ["xarg"] =  {
-          }
-          ["label"] = b
-          ["id"] = 90
-        }
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = button
-        ["id"] = 89
-      }
-      [19] =  {
-        ["xarg"] =  {
-        }
-        ["label"] = br
-        ["id"] = 92
-      }
-      [20] =  {
-        [1] =  {
-          ["dirty"] = true
-          ["xarg"] =  {
-            ["src"] = projects/browser/data/images/image-button1.png
-            ["height"] = 60
-            ["width"] = 104
-            ["alt"] = imageButton
-          }
-          ["label"] = img
-          ["id"] = 94
-        }
-        ["dirty"] = true
-        ["xarg"] =  {
-        }
-        ["label"] = button
-        ["id"] = 93
-      }
-      [21] =  {
-        ["xarg"] =  {
-        }
-        ["label"] = br
-        ["id"] = 95
-      }
-      [22] =  {
-        ["xarg"] =  {
-        }
-        ["label"] = br
-        ["id"] = 96
-      }
-      [23] =  {
-        ["dirty"] = true
-        ["xarg"] =  {
-          ["src"] = projects/browser/data/images/sample1.png
-          ["height"] = 142
-          ["width"] = 164
-          ["alt"] = imageCar
-        }
-        ["label"] = img
-        ["id"] = 97
-      }
-      [24] =  {
-        [1] = Your browser does not support the canvas tag.
-        ["id"] = 98
-        ["dirty"] = true
-        ["label"] = canvas
-        ["xarg"] =  {
-          ["id"] = myCanvas
-        }
-      }
-      ["id"] = 5
-      ["dirty"] = true
       ["label"] = body
+      ["dirty"] = true
       ["xarg"] =  {
       }
+      ["id"] = 5
     }
+    ["label"] = html
     ["dirty"] = true
     ["xarg"] =  {
     }
-    ["label"] = html
     ["id"] = 2
   }
+  ["label"] = #document
   ["dirty"] = true
   ["xarg"] =  {
   }
-  ["label"] = #document
   ["id"] = 1
 }
